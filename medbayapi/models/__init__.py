a382b934cc473ab5714629410204cc31c0a12fe7
Jeremiah King <usaxray25@gmail.com>
2023-02-14 21:16:15 -0600

    custom events for m2m table complete and postman verified working

--- medbayapi/models/__init__.py
+++ medbayapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .users import User
+from .physician_user import PhysicianUser
 from .physicians import Physician
 from .records import Record
--- medbayapi/views/physician.py
+++ medbayapi/views/physician.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers, status
-from medbayapi.models import Physician, Record, User
+from rest_framework import status
+from medbayapi.models import Physician, Record, User, PhysicianUser
 from medbayapi.serializers import PhysicianSerializer
 from rest_framework.decorators import action
 
@@ -23,6 +23,7 @@ class PhysicianView(ViewSet):
     def create(self, request):
         """Handle create requests for physician
         """
+        record = Record.objects.get(pk=request.data["record"])
         user = User.objects.get(pk=request.data["user"])
 
         physician = Physician.objects.create(
@@ -31,6 +32,7 @@ class PhysicianView(ViewSet):
                 email=request.data["email"],
                 location=request.data["location"],
                 phone_number=request.data["phone_number"],
+                record=record,
                 user=user
             )
         serializer = PhysicianSerializer(physician)
@@ -44,6 +46,9 @@ class PhysicianView(ViewSet):
         physician.email=request.data["email"]
         physician.location=request.data["location"]
         physician.phone_number=request.data["phone_number"]
+
+        record = Record.objects.get(pk=request.data["record"])
+        physician.record=record
         physician.save()
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)
@@ -52,3 +57,29 @@ class PhysicianView(ViewSet):
         physician = Physician.objects.get(pk=pk)
         physician.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    @action(methods=['post'], detail=True)
+    def assign(self, request, pk):
+        """Post request for a user to sign up for an physician"""
+
+        physician = Physician.objects.get(pk=pk)
+        user = User.objects.get(pk=request.data["user_id"])
+        PhysicianUser.objects.create(
+            user=user,
+            physician=physician
+        )
+
+        return Response({'message': 'User added'}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['delete'], detail=True)
+    def remove(self, request, pk):
+        """Delete request for a user to leave an physician"""
+
+        physician = Physician.objects.get(pk=pk)
+        user = User.objects.get(pk=request.data["user_id"])
+        physician_user = PhysicianUser.objects.filter(
+            user=user,
+            physician=physician
+        )
+        physician_user.delete()
+        return Response({'message': 'User removed'}, status=status.HTTP_204_NO_CONTENT)
